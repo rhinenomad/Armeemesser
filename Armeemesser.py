f371287e4a502ecc05517e3fd73a2f853334fcf3
FlyingheartCN <feng591892871@gmail.com>
2019-02-25 23:05:55 +0800

    add Statistic

--- Armeemesser.py
+++ Armeemesser.py
@@ -3,6 +3,21 @@ import argparse
 import string
 import config
 
+class Statistic(object):
+    success_task = []
+    success_num = 0
+    failed_num = 0
+    failed_task=[]
+    def success(self,name):
+        self.success_num += 1
+        self.success_task.append(name)
+    def fail(self,name):
+        self.failed_num += 1
+        self.failed_task.append(name)
+    def show_result(self):
+        print(self.success_task)
+        print(self.failed_task)
+        pass
 
 def plog(str,level=0):
     if level==0:
@@ -16,6 +31,8 @@ def task_runner(task,variable,url):
     plog(task)
     plog(variable)
     plog(url)
+    TaskStatistic.success(task['name'])
+    TaskStatistic.fail(task['name'])
     pass
 
 def main():
@@ -27,13 +44,14 @@ def main():
         config.file = args.file
     if args.url:
         config.url = args.url
-    plog(config.file)
     test_script = yaml.load(open(config.file))
     variable = test_script['vars']
     tasks=test_script['tasks']
     for task in tasks:
         task_runner(task,variable,config.url)
     pass
+    TaskStatistic.show_result()
 
+TaskStatistic = Statistic()
 if __name__ == '__main__':
     main()
